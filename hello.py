f734714d5ce7e4cb5c91f135c15e1a789c69ff79
Alex Zhang <zhangxiaojie@cynovo.com.cn>
2017-11-27 11:10:27 +0800

    Chapter 6: Asynchronous emails

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,7 +25,7 @@ app.config['MAIL_PORT'] = 25
 app.config['MAIL_USE_TLS'] = True
 # app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') or 'jience520@163.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') or '#############' # 此处为客户端授权码
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') or 'Jie137066162'  # 此处为客户端授权码
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
 app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <jience520@163.com>'
 app.config['FLASK_ADMIN'] = os.environ.get('FLASKY_ADMIN') or '460568740@qq.com'
@@ -37,12 +38,19 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 @app.route('/', methods=['GET', 'POST'])
