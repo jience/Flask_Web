91c3045fdd0a64980519c0177e8a0d7ca7cccc88
Alex Zhang <zhangxiaojie@cynovo.com.cn>
2017-11-28 17:55:07 +0800

    Chapter 8: Authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -23,5 +23,8 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
