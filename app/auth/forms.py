c4d88040ac6bd485fdd457c0157b36028f03f370
Alex Zhang <zhangxiaojie@cynovo.com.cn>
2017-11-29 13:47:30 +0800

    Chapter 8: Password resets

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -40,3 +40,16 @@ class ChangePasswordForm(FlaskForm):
         DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm new password', validators=[DataRequired()])
     submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password', validators=[DataRequired()])
+    submit = SubmitField('Update Password')
