6b0d36c59b61b50030b3919f8e2441bfc2646a56
Alex Zhang <zhangxiaojie@cynovo.com.cn>
2017-12-06 13:15:09 +0800

    Chapter 12: Followers in the application

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <jience520@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or '460568740@qq.com'
     FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
