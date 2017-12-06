12e4bd21bdc5c6a6ba1e5285df5f16f97b923337
Alex Zhang <zhangxiaojie@cynovo.com.cn>
2017-12-06 14:07:43 +0800

    Chapter 12: Show followed blog posts in home page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, abort, flash, \
-    redirect, url_for, request, current_app
+    redirect, url_for, request, current_app, make_response
 from app import db
 from app.decorators import admin_required, permission_required
 from app.models import User, Role, Permission, Post
@@ -17,12 +17,19 @@ def index():
         db.session.add(post)
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts,
-                           pagination=pagination)
+                           show_followed=show_followed, pagination=pagination)
 
 
 @main.route('/user/<username>')
@@ -116,6 +123,7 @@ def follow(username):
         flash('You are already following this user.')
         return redirect(url_for('.user', username=username))
     current_user.follow(user)
+    db.session.commit()
     flash('You are now following %s.' % username)
     return redirect(url_for('.user', username=username))
 
@@ -132,6 +140,7 @@ def unfollow(username):
         flash('You are not following this user.')
         return redirect(url_for('.user', username=username))
     current_user.unfollow(user)
+    db.session.commit()
     flash('You are not following %s anymore.' % username)
     return redirect(url_for('.user'), username=username)
 
@@ -168,3 +177,19 @@ def followed_by(username):
     return render_template('followers.html', user=user, title="Followed by",
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
+
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return resp
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
