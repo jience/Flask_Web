52343671dbd3424cbc56dfddefa4e77153865eda
Alex Zhang <zhangxiaojie@cynovo.com.cn>
2017-12-07 20:24:14 +0800

    Chapter 15: Unit tests with Selenium

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,17 @@ from app.main.forms import EditProfileForm, EditProfileAdminForm, \
     PostForm, CommentForm
 
 
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
